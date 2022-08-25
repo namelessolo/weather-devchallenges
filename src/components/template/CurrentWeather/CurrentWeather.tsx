99d8770c50fb46406444ba832a1c913b25425a87
Aleksander <aleksader.skorek@gmail.com>
2022-08-25 19:20:03 +0200

    somethig fucked up with git

--- src/components/template/CurrentWeather/CurrentWeather.tsx
+++ src/components/template/CurrentWeather/CurrentWeather.tsx
@@ -1,15 +1,25 @@
+import { useState } from 'react';
+
 import SearchPlacesButton from '../../atoms/SearchPlacesButton/SearchPlacesButton';
 import LocationButton from '../../atoms/LocationButton/LocationButton';
 import CurrentWeatherImage from '../../atoms/CurrentWeatherImage/CurrentWeatherImage';
+import Menu from '../Menu/Menu';
 
 const CurrentWeather = () => {
+  const [menu, setmenu] = useState(false);
   return (
     <div className='bg-front h-screen text-white px-3 pt-5'>
-      <div className='flex justify-between items-center'>
-        <SearchPlacesButton />
-        <LocationButton />
-      </div>
-      <CurrentWeatherImage />
+      {menu ? (
+        <Menu />
+      ) : (
+        <>
+          <div className='flex justify-between items-center'>
+            <SearchPlacesButton />
+            <LocationButton />
+          </div>
+          <CurrentWeatherImage />
+        </>
+      )}
     </div>
   );
 };

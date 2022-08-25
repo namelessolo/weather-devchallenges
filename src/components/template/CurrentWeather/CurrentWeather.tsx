8eed27c4a1758e89871d6b239642436e13d37c60
Aleksander <aleksader.skorek@gmail.com>
2022-08-25 19:20:03 +0200

    something went wrong with git

--- src/components/template/CurrentWeather/CurrentWeather.tsx
+++ src/components/template/CurrentWeather/CurrentWeather.tsx
@@ -6,10 +6,16 @@ import CurrentWeatherImage from '../../atoms/CurrentWeatherImage/CurrentWeatherI
 import Menu from '../Menu/Menu';
 
 const CurrentWeather = () => {
+<<<<<<< HEAD
   const [menu, setMenu] = useState(false);
 
   return (
     <div className='bg-front h-screen text-white px-3 pt-5 min-h-[820px]'>
+=======
+  const [menu, setmenu] = useState(false);
+  return (
+    <div className='bg-front h-screen text-white px-3 pt-5'>
+>>>>>>> 99d8770... somethig fucked up with git
       {menu ? (
         <Menu />
       ) : (
